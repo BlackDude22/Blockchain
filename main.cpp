f1b68442cae3dacd92b1477d3c90d64f9aaf2d93
BlackDude22 <lukislt7@gmail.com>
2019-10-18 11:04:10 +0300

    v0.1.1

--- main.cpp
+++ main.cpp
@@ -15,17 +15,22 @@ int main()
     Users& u = Users::getInstance();
     BlockChain& bc = BlockChain::getInstance();
 
+    std::cout << "Creating users..." << std::endl;
     for (int i = 0; i < USER_COUNT; i++)
         u.createNewUser("name"+std::to_string(i), "public_key"+std::to_string(i), rand()%10 + 40);
-
+    std::cout << "Users created." << std::endl;
+    std::cout << "Creating transactions..." << std::endl;
     for (int i = 0; i < TRANSACTION_COUNT; i++){
         std::string pk1 = u.getPublicKey(rand()%USER_COUNT);
         std::string pk2 = u.getPublicKey(rand()%USER_COUNT);
-        tp.addNewTransaction(pk1, rand()%10, pk2, 0);
+        tp.addNewTransaction(pk1, rand()%10 + 1, pk2, 0);
     }
-    
-    std::cout << "Current balance: " <<  u.getUser("public_key0").getBalance() << std::endl;
+    std::cout << "Transaction created." << std::endl;
+    std::string trackedPK = "public_key0";
+    std::cout << "Current balance(" << trackedPK << "): " <<  u.getUser(trackedPK).getBalance() << std::endl;
     for (int i = 0; i < 100; i++){
+        std::cout << std::endl;
+        std::cout << "--- Mining block ---" << std::endl;
         Block b(bc.getLastBlockHash());
         b.mine();
         if (bc.addBlock(b)){
@@ -35,10 +40,18 @@ int main()
             std::cout << "Merkle root hash:    " << b.getMerkleRootHash() << std::endl;
             std::cout << "Nonce:               " << b.getNonce() << std::endl;
             std::cout << "Timestamp:           " << b.getTimeStamp() << std::endl;
-            std::cout << "Transaction count:   " << b.getTransactions().size() << std::endl;
+            std::cout << "Current balance(" << trackedPK << "): " <<  u.getUser(trackedPK).getBalance() << std::endl;
+            std::cout << "Sent transactions: " << std::endl;
+            for (Transaction t : b.getTransactions())
+                if (t.getSenderPublicKey() == trackedPK)
+                    std::cout << t.getTransactionString() << std::endl;
+            std::cout << "Recieved transactions: " << std::endl;
+            for (Transaction t : b.getTransactions())
+                if (t.getRecieverPublicKey() == trackedPK)
+                    std::cout << t.getTransactionString() << std::endl;
         }
         else 
             std::cout << "Block rejected" << std::endl;
-        std::cout << "Current balance: " <<  u.getUser("public_key0").getBalance() << std::endl;
+        std::cout << "--- Block mined ---" << std::endl;
     }  
 }
